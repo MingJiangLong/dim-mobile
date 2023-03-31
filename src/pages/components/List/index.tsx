5921c2ce460c99b5a9568e2c73a872bc2c76e4a3
longjiang <l502858283@gmail.com>
2023-03-31 15:49:00 +0800

    过略系统菜单

--- src/pages/components/List/index.tsx
+++ src/pages/components/List/index.tsx
@@ -1,17 +1,25 @@
 import { useEffect, useMemo, useState } from "react"
 import Empty from "../Empty"
 import MainList from "../MainList"
+import NoAccess from "../NoAccess"
 
 export default function () {
   const [menusData, setMenuData] = useState<any>()
   const menuList = useMemo(() => {
     const data = menusData?.menu_list
-    if (Array.isArray(data)) return data.filter(item => item?.type == 1)
+    if (Array.isArray(data))
+      return data.filter(
+        item =>
+          item?.type == 1 && !`${item?.method ?? ""}`.startsWith("/system")
+      )
     return []
   }, [menusData])
+  const [haveAccess, setHaveAccess] = useState(false)
+
   useEffect(() => {
     // @ts-ignore
     const data = window?.data || {}
+    setHaveAccess(data?.code == 200)
     setMenuData(data)
   }, [])
 
@@ -19,8 +27,10 @@ export default function () {
     <>
       {menuList.length ? (
         menuList.map((item, index) => <MainList {...item} key={index} />)
-      ) : (
+      ) : haveAccess ? (
         <Empty />
+      ) : (
+        <NoAccess />
       )}
     </>
   )
